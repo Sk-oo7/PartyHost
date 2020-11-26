b7bfce3aba8988f24b142d95fdd63c8eaa106500
Mayank Nagpal <mayanknagpal.nm@gmail.com>
2020-11-26 13:26:57 +0530

    Initialize Spring Boot Application

---
+++ server/partyhost/src/test/java/com/partyhost/PartyHostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.partyhost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyHostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
