e922e8d796e3daae2caa6495ec753ea3bfed2fec
Mayank Nagpal <mayanknagpal.nm@gmail.com>
2020-11-26 14:46:50 +0530

    Added additional info to table details file and added UserAlreadyExists Exception

--- server/partyhost/src/main/java/com/partyhost/controller/UsersController.java
+++ server/partyhost/src/main/java/com/partyhost/controller/UsersController.java
@@ -1,10 +1,13 @@
 package com.partyhost.controller;
 
+import com.partyhost.exception.UserAlreadyExistsException;
 import com.partyhost.model.Users;
 import com.partyhost.repository.UsersRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -13,8 +16,18 @@ public class UsersController {
     @Autowired
     private UsersRepository usersRepository;
 
+    private List<Users> getAllUsers() {
+        return usersRepository.findAll();
+    }
+
     @PostMapping("/user/signup")
     public Users createUser(@RequestBody Users user) {
+        List<Users> users = getAllUsers();
+        for (Users tempUser: users) {
+            if(user.getEmailId().equals(tempUser.getEmailId())) {
+                throw new UserAlreadyExistsException();
+            }
+        }
         return usersRepository.save(user);
     }
 
