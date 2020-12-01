6058c2abc3b9164f6f81f097fa2ca407d62820ed
Muskan <69676535+MuskanDoomra@users.noreply.github.com>
2020-12-01 22:54:28 +0530

    Added login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+
+import {BrowserRouter as Router,Route, Switch} from 'react-router-dom'
+
+import LoginPage from './screens/login/LoginPage';
+import StartPage from './screens/start/StartPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path="/login"><LoginPage /></Route>
+          <Route path="/start"><StartPage /></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ client/src/components/login/Slider.js
@@ -0,0 +1,64 @@
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import React from 'react'
+import "./styles.css"
+
+function Slider() {
+    const responsive = {
+        superLargeDesktop: {
+          breakpoint: { max: 4000, min: 3000 },
+          items: 1
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 1
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 1
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1
+        }
+      };
+    return (
+        <div class="slider">
+            <Carousel 
+                showDots={true}
+                responsive={responsive}
+                infinite={true}
+            >
+                <div>
+                    <img src="money_notes.png" className="slider_img"/>
+                    <center>
+                        <p className="slider_para top_slider_para">Never again worry about your lendings / borrowings.</p>
+                        <p className="slider_para lower_slider_para">Start using PartyHost, your own online ledger.</p>
+                        <br /> <br />
+                    </center>
+                </div>
+                <div>
+                    <img src="party_icon.png" className="slider_img"/>
+                    <center>
+                        <p className="slider_para top_slider_para">You can create groups with various friends and divide up what each person owes</p>
+                        <p className="slider_para">All your shared expenses and IOUs are organized in one place and everyone in the group can see the amount they owe.</p>
+                    </center>
+                </div>
+                <div>
+                    <img src="borrowing_icon.png" className="slider_img" style={{height: "250px"}}/>
+                    <center>
+                        <p className="slider_para top_slider_para" style={{marginTop: "13%"}}>It tracks borrowing and lending over time.</p>
+                        <p className="slider_para">And, until the balance is settled up, each person’s outstanding balance may go up and down.</p>
+                    </center>
+                </div>
+                <div>
+                    <img src="clock_icon.png" className="slider_img"/>
+                    <center>
+                        <p className="slider_para top_slider_para">PartyHost sends reminders at the end of the month so everyone can pay what they owe and go into a new month with a fresh start.</p>
+                    </center>
+                </div>
+            </Carousel>
+        </div>
+    )
+}
+export default Slider
